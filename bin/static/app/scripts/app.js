705cf4ae824458e0029b62c41f9a6fefafb83ba5
Rakesh Nair <nair.fm@gmail.com>
2016-02-09 21:28:41 -0500

    init

---
+++ bin/static/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webappApp
+ * @description
+ * # webappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webappApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.bootstrap',
+    'datatables'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/subscriber', {
+        templateUrl: 'views/subscriber.html',
+        controller: 'SubscriberCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
