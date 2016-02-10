e94a363c8cf2f144bd6c8314f61fd8ce1f06e0b7
Rakesh Nair <nair.fm@gmail.com>
2016-02-09 21:40:59 -0500

    init

---
+++ DataPipelineGateway/bin/static/app/scripts/controllers/about.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webappApp
+ */
+angular.module('webappApp')
+  .controller('AboutCtrl', function ($scope) {
+	  console.log('hi about')
+  });
